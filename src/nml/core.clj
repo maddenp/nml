d90929d6d1a76978d2674959a53b2d15f8962e4c
Paul Madden <maddenp@colorado.edu>
2014-07-31 13:37:47 -0600

    more tweaking

--- src/nml/core.clj
+++ src/nml/core.clj
@@ -16,36 +16,40 @@
 ;;   (println (str "k=" k " v=" v))
     (apply str
            (case k
+             :s        (delegate v)
              :array    [(sf v) (sl v)]
              :c        (sf v)
              :colon    ":"
+             :comma    ","
              :comment  ""
              :complex  ["(" (cjoin (delegate v)) ")"]
              :dataref  (delegate v)
              :dec      (delegate v)
              :dot      "."
+             :exp      [(first v) (sl v)]
              :false    "f"
              :int      (delegate v)
              :logical  (sf v)
              :name     (map string/lower-case v)
-             :nvchoice (sf v)
-             :nvpair   (delegate v)
              :nvseq    (delegate v)
              :nvsubseq ["  " (sf v) "=" (sl v) "\n"]
              :partref  (sf v)
              :percent  "%"
              :r        (sf v)
              :real     (delegate v)
-             :s        (delegate v)
              :sect     ["(" (list2str v) ")"]
+             :sep      v
              :sign     v
+             :slash    v
              :star     "*"
              :stmt     ["&" (sf v) "\n" (list2str (rest v)) "/\n"]
              :string   v
              :true     "t"
              :uint     v
              :value    (delegate v)
-             :values   (cjoin (delegate v))))))
+             :values   (cjoin (delegate v))
+             :ws       ""
+             :wsopt    ""))))
 
 (defn -main
   [& args]
