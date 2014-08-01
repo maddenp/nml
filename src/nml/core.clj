ec7bddce7550328f7cdaea6aa06a39a5689fe821
Paul Madden <maddenp@colorado.edu>
2014-08-01 15:31:22 -0600

    cosmetics

--- src/nml/core.clj
+++ src/nml/core.clj
@@ -5,6 +5,9 @@
 
 (def debug false)
 
+(defn nmlget [] )
+(defn nmlset [] )
+
 (def parse (insta/parser (clojure.java.io/resource "grammar")))
 
 (defn prpr [x]
@@ -17,45 +20,45 @@
         sf       #(prpr (first %))
         sl       #(prpr (last %))]
     (if debug (println (str "k=" k " v=" v)))
-    (apply str
-           (case k
-             :s        (ds v)
-             :array    [(sf v) (sl v)]
-             :c        (sf v)
-             :colon    ":"
-             :comma    ","
-             :comment  ""
-             :complex  ["(" (cjoin (delegate v)) ")"]
-             :dataref  (delegate v)
-             :dec      (delegate v)
-             :dot      "."
-             :exp      [(first v) (sl v)]
-             :false    "f"
-             :int      (delegate v)
-             :junk     ""
-             :logical  (sf v)
-             :name     (map string/lower-case v)
-             :nvseq    (ds v)
-             :nvsubseq ["  " (sf v) "=" (sl v) "\n"]
-             :partref  (sf v)
-             :percent  "%"
-             :r        (sf v)
-             :real     (delegate v)
-             :sect     ["(" (list2str v) ")"]
-             :sep      v
-             :sign     v
-             :slash    v
-             :star     "*"
-             :stmt     ["&" (sf v) "\n" (list2str (rest v)) "/\n"]
-             :string   v
-             :true     "t"
-             :uint     v
-             :value    (delegate v)
-             :values   (cjoin (delegate v))
-             :ws       ""
-             :wsopt    ""))))
+    (apply str (case k
+                 :s        (ds v)
+                 :array    [(sf v) (sl v)]
+                 :c        (sf v)
+                 :colon    ":"
+                 :comma    ","
+                 :comment  ""
+                 :complex  ["(" (cjoin (delegate v)) ")"]
+                 :dataref  (delegate v)
+                 :dec      (delegate v)
+                 :dot      "."
+                 :exp      [(first v) (sl v)]
+                 :false    "f"
+                 :int      (delegate v)
+                 :junk     ""
+                 :logical  (sf v)
+                 :name     (map string/lower-case v)
+                 :nvseq    (ds v)
+                 :nvsubseq ["  " (sf v) "=" (sl v) "\n"]
+                 :partref  (sf v)
+                 :percent  "%"
+                 :r        (sf v)
+                 :real     (delegate v)
+                 :sect     ["(" (list2str v) ")"]
+                 :sep      v
+                 :sign     v
+                 :slash    v
+                 :star     "*"
+                 :stmt     ["&" (sf v) "\n" (list2str (rest v)) "/\n"]
+                 :string   v
+                 :true     "t"
+                 :uint     v
+                 :value    (delegate v)
+                 :values   (cjoin (delegate v))
+                 :ws       ""
+                 :wsopt    ""))))
 
 (defn -main [& args]
   (alter-var-root #'*read-eval* (constantly false))
-  (let [namelist-file (last args)]
-    (println (prpr (parse (slurp namelist-file))))))
+  (let [namelist-file (last args)
+        tree (parse (slurp namelist-file))]
+    (println (prpr tree))))
