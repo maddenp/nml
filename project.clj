7834846312df55e17dae9a2a8e15ba4cfd2cea28
Paul Madden <maddenp@colorado.edu>
2014-08-05 22:05:43 -0600

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
-(defproject nml "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[instaparse "1.3.2"]
-                 [org.clojure/clojure "1.6.0"]]
-  :main nml.core)
+(defproject nml "0.0"
+  :aot [nml.core]
+  :dependencies [[instaparse "1.3.2"] [org.clojure/clojure "1.6.0"]]
+  :main nml.core
+  :url "https://github.com/maddenp/nml")
