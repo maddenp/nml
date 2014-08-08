abb5ca9f0cac02165a70986d4e83ab45f90936c7
Paul Madden <maddenp@colorado.edu>
2014-08-08 14:50:43 -0600

    simple uberjar name

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject nml "0.0"
+(defproject nml "0.1"
   :aot [nml.core]
   :dependencies [[instaparse "1.3.2"] [org.clojure/clojure "1.6.0"] [org.clojure/tools.cli "0.3.1"]]
+  :description "A query/modify utility for Fortran namelists"
   :main nml.core
+  :uberjar-name "nml.jar"
   :url "https://github.com/maddenp/nml")
