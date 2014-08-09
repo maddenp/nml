a3f67af9683a8ee7110eb9218930e27e800bda46
Paul Madden <maddenp@colorado.edu>
2014-08-09 10:07:46 -0600

    work on tests

--- test/nml/core_test.clj
+++ test/nml/core_test.clj
@@ -2,6 +2,31 @@
   (:require [clojure.test :refer :all]
             [nml.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(let [tree (nml-tree "test/nml/nl")]
+
+  (deftest character
+    (is (= (nml-get tree "na" "C0") "'foo'"   ))
+    (is (= (nml-get tree "NA" "c1") "'bar'"   ))
+    (is (= (nml-get tree "nA" "c2") "\"baz\"" )))
+
+  (deftest integer
+    (is (= (nml-get tree "nb" "I0") "88"      ))
+    (is (= (nml-get tree "nb" "I1") "+88"     ))
+    (is (= (nml-get tree "NB" "i2") "-88"     ))
+    (is (= (nml-get tree "nb" "i3") ""        )))
+
+  (deftest logical
+    (is (= (nml-get tree "NC" "t0") "t"       ))
+    (is (= (nml-get tree "NC" "t1") "t"       ))
+    (is (= (nml-get tree "NC" "t2") "t"       ))
+    (is (= (nml-get tree "NC" "t3") "t"       ))
+    (is (= (nml-get tree "NC" "t4") "t"       ))
+    (is (= (nml-get tree "NC" "t5") "t"       ))
+    (is (= (nml-get tree "nc" "f0") "f"       ))
+    (is (= (nml-get tree "nc" "f1") "f"       ))
+    (is (= (nml-get tree "nc" "f2") "f"       ))
+    (is (= (nml-get tree "nc" "f3") "f"       ))
+    (is (= (nml-get tree "nc" "f4") "f"       ))
+    (is (= (nml-get tree "nc" "f5") "f"       )))
+  )
