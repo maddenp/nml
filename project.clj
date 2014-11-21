25a6fcfb63da05b54e56904f6779d2219ec7da3a
Paul Madden <maddenp@colorado.edu>
2014-11-21 12:25:36 -0700

    add license to project.clj

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :aot [nml.core]
   :dependencies [[instaparse "1.3.2"] [org.clojure/clojure "1.6.0"] [org.clojure/tools.cli "0.3.1"]]
   :description "A query/modify utility for Fortran namelists"
+  :license {:name "Apache License Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :main nml.core
   :uberjar-name "nml.jar"
   :url "https://github.com/maddenp/nml")
