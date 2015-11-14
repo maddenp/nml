00c88f69d93fc23d6391dc11d24b256294b72f61
Paul Madden <maddenp@colorado.edu>
2015-11-13 17:10:07 -0700

    Update cli to 0.33

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject nml "0.1"
   :aot [nml.core]
-  :dependencies [[instaparse "1.4.1"] [org.clojure/clojure "1.7.0"] [org.clojure/tools.cli "0.3.1"]]
+  :dependencies [[instaparse "1.4.1"] [org.clojure/clojure "1.7.0"] [org.clojure/tools.cli "0.3.3"]]
   :description "A query/modify utility for Fortran namelists"
   :license {:name "Apache License Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :main nml.core
