a4f4b0d4315bb207f30e4916ea107741152b3f66
Paul Madden <maddenp@colorado.edu>
2015-11-15 11:27:43 -0700

    In test.f90: test that var absent from namelist does not affect default value

--- test/nml/test.f90
+++ test/nml/test.f90
@@ -3,7 +3,7 @@ program p
   character(len=3) :: c0 = '', c1 = '', c2 = '', c3 = ''
   character(len=1) :: c4(2) = '', c5(2) = ''
   character(len=1) :: x = 'x'
-  integer :: i0 = 0, i1 = 0, i2 = 0
+  integer :: i0 = 0, i1 = 0, i2 = 0, i3 = 77
   logical :: t0 = .false., t1 = .false., t2 = .false., t3 = .false., t4 = .false., t5 = .false.
   logical :: f0 = .true., f1 = .true., f2 = .true., f3 = .true., f4 = .true., f5 = .true.
   complex :: m0 = (0, 0), m1 = (0, 0), m2 = (0, 0), m3 = (0, 0)
@@ -33,6 +33,7 @@ program p
   if (i0.ne.88)             stop 'FAIL i0'
   if (i1.ne.88)             stop 'FAIL i1'
   if (i2.ne.-88)            stop 'FAIL i2'
+  if (i3.ne.77)             stop 'FAIL i3'
   if (t0.neqv..true.)       stop 'FAIL t0'
   if (t1.neqv..true.)       stop 'FAIL t1'
   if (t2.neqv..true.)       stop 'FAIL t2'
