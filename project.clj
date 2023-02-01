9787a4bbee182b8d387a9f0810cea07b060d30c4
Paul Madden <maddenp@colorado.edu>
2023-02-01 11:54:38 +0000

    Package updates, etc.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject nml "1.0.1"
+(defproject nml "1.1.0"
   :aot [nml.core]
-  :dependencies [[instaparse "1.4.9"]
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/tools.cli "0.3.7"]]
+  :dependencies [[com.github.clj-easy/graal-build-time "0.1.4"]
+                 [instaparse "1.4.12"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/tools.cli "1.0.214"]]
   :description "A query/modify utility for Fortran namelists"
   :license {:name "Apache License Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :main nml.core
